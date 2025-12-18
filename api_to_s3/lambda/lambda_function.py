c9cf8496d5dc351d45313691b71957a51c4af2cf
gilsegev99 <gilsegev@LAPTOP-0JJ0R1CI.>
2025-12-18 08:57:20 +0000

    First fully working lambda function with correct timeout limit and easily updateable code (using source hash)

--- api_to_s3/lambda/lambda_function.py
+++ api_to_s3/lambda/lambda_function.py
@@ -11,7 +11,8 @@ VIEWS_BASE_URL = "https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wik
 WIKIPEDIA_BASE_URL = "https://en.wikipedia.org/w/api.php"
 HEADERS = {"User-Agent": os.environ["USER_AGENT_STRING"]}
 
-yesterday = (datetime.now(timezone.utc) - timedelta(days=1)).strftime('%Y/%m/%d')
+yesterday_slash_fmt = (datetime.now(timezone.utc) - timedelta(days=1)).strftime('%Y/%m/%d')
+yesterday_dash_fmt = (datetime.now(timezone.utc) - timedelta(days=1)).strftime('%Y-%m-%d')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -48,20 +49,20 @@ def get_categories(article):
 
 def lambda_handler(event, context):
 
-    views_data = get_pageviews(yesterday)
-    views_key = f"raw/views_data/{yesterday}.json"
+    views_data = get_pageviews(yesterday_slash_fmt)
+    views_key = f"raw/views_data/{yesterday_dash_fmt}.json"
 
     if views_data is not None:
         try:
             s3_client.put_object(
-                Bucket=os.environ["BUCKET"],
+                Bucket=os.environ["BUCKET_NAME"],
                 Key=views_key,
                 Body=json.dumps(views_data)
             )
 
-            logger.info(f"Successfully uploaded {yesterday} views data to S3.")
+            logger.info(f"Successfully uploaded {yesterday_slash_fmt} views data to S3.")
         except Exception as e:
-            logger.error(f"Failed to upload {yesterday} views data to S3: {str(e)}")
+            logger.error(f"Failed to upload {yesterday_slash_fmt} views data to S3: {str(e)}")
             raise
     
     article_titles = [article['article'] for article in views_data['items'][0]['articles']]
@@ -73,7 +74,7 @@ def lambda_handler(event, context):
         if article_categories is not None:
             try:
                 s3_client.put_object(
-                    Bucket=os.environ["BUCKET"],
+                    Bucket=os.environ["BUCKET_NAME"],
                     Key=cat_key,
                     Body=json.dumps(article_categories)
                 )
@@ -84,5 +85,5 @@ def lambda_handler(event, context):
 
     return {
             "statusCode": 200,
-            "message": f"{yesterday} data processed successfully"
+            "message": f"{yesterday_slash_fmt} data processed successfully"
         }
